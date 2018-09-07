24138354de529b6fafb182b7dafac5c68c50bcbc
Anna Kowalczuk <anna@kowalczuk.eu>
2018-09-07 23:48:30 +0200

    Add script serving cat.jpg on 404

--- index.js
+++ index.js
@@ -5,17 +5,22 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/index.html') {
         fs.readFile('./index.html', 'utf-8', function (err, data) {
             if (err) throw err;
+            response.setHeader("Content-Type", "text/html; charset=utf-8;");
             response.write(data);
             response.end();
         });
     } else {
         response.statusCode = 404;
-        response.write('<h1>404: Zła ścieżka!</h1>');
-        response.end();
+        fs.readFile('./cat.jpg', function (err, data) {
+            console.log(data);
+            response.setHeader("Content-Type", "image/jpeg;");
+            if (err) throw err;
+            response.write(data);
+            response.end();
+        });
     }
 });
 
